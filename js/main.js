6f8fdcb4e515648e07991186498867b6f72b9bf5
SIARHEI TSYKHUN <it4money@gmail.com>
2022-03-28 10:13:05 +0300

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,43 @@
 let welcome = "start project";
 alert(welcome);
+
+/*
+object #1
+*/
+
+const questionOne = {
+    question: 'Вопрос',
+    answer: 'Ответ',
+    correctAnswer: 'Правильный Ответ'
+};
+
+
+/*
+object #2
+*/
+
+const questionTwo = {
+    question: 'Вопрос',
+    answer: 'Ответ',
+    correctAnswer: 'Правильный Ответ'
+};
+
+/*
+object #3
+*/
+
+const questionThree = {
+    question: 'Вопрос',
+    answer: 'Ответ',
+    correctAnswer: 'Правильный Ответ'
+};
+
+/*
+object #4
+*/
+
+const questionFour = {
+    question: 'Вопрос',
+    answer: 'Ответ',
+    correctAnswer: 'Правильный Ответ'
+};
